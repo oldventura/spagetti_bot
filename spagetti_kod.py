b9b7d0f7c1f9f401e4d3a7447b630d3cd547ea4f
kerbalgalactic <98157790+kerbalgalactic@users.noreply.github.com>
2022-01-29 21:24:34 +0300

    Update spagetti_kod.py

--- spagetti_kod.py
+++ spagetti_kod.py
@@ -1,10 +1,8 @@
 import praw
-import pdb
 import os
 import sys
 import time
 import openai
-import hashlib
 import random
 import deep_translator
 from deep_translator import GoogleTranslator
@@ -33,24 +31,29 @@ non_bmp_map = dict.fromkeys(range(0x10000, sys.maxunicode + 1), 0xfffd)
 trans = lambda x: x.translate(non_bmp_map)
 
 def ask(question, chat_log=None):
-    sampleList = ["davinci", "ada", "babbage", "curie"]
     person = "text-davinci-001"
     if chat_log is None:
         chat_log = start_chat_log
-    prompt = f'{chat_log}Human: {question}\nAI:'
+    prompt = f'{chat_log}Human 1: {question}\nHuman 2:'
     response = completion.create(
-        prompt=prompt, engine=person, stop=['\nHuman'], temperature=0.5,
-        top_p=1, frequency_penalty=1, presence_penalty=0.4, best_of=1,
-        max_tokens=800)
+        prompt=prompt, engine=person, stop=['\nHuman'], temperature=0.9,
+        top_p=1, frequency_penalty=0, presence_penalty=0, best_of=1,
+        max_tokens=150)
     answer = response.choices[0].text.strip()
     if "\nAI:" in answer:
         answer = answer.split("\nAI:")[0]
+    if "Human 1:" in answer:
+        answer = answer.split("Human 1:")[0]
+    if "Human 2:" in answer:
+        answer = answer.split("Human 2:")[0]
     if "Human:" in answer:
         answer = answer.split("Human:")[0]
     return answer
 
 def slangify(text):
     sent = text.split(".")
+    if len(sent) < 2:
+        return "".join(sent)
     slang = [" amk."," aq."]
     cons = ""
     for i in sent:
@@ -107,17 +110,19 @@ while True:
                     except AttributeError:
                         continue
                     except deep_translator.exceptions.NotValidPayload:
-                        out = "..."
+                        out = ". . ."
                         item.reply(out)
                         posts_replied_to.append(i.id)
                         continue
                     except deep_translator.exceptions.NotValidLength:
-                        out = "Bu ne amk çok uzun"
+                        out = "##bu ne amk çok uzun"
                         item.reply(out)
                         posts_replied_to.append(i.id)
                         continue
+                    out = out.lower()
                     out = out.replace("girişiniz için teşekkür ederiz","geri dönütünüz için teşekkür ederiz")
                     out = out.replace("ai ", "yapay zeka ")
+                    out = out.replace(" ai", " yapay zeka")
                     out = out.replace("numara", "hayır")
                     out = slangify(out.replace("Numara", "Hayır"))
                     posts_replied_to.append(i.id)
@@ -127,12 +132,12 @@ while True:
             for item in reddit.inbox.unread(limit=None):
                 print("Replying to:",trans(item.body))
                 if "good bot" in item.body.lower():
-                    item.reply("Sende iyi insansın amk")
+                    item.reply("sende iyi insansın amk")
                 elif "bad bot" in item.body.lower():
-                    item.reply("Kes lan amk soran mı oldu >:/")
+                    item.reply("kes lan amk soran mı oldu >:/")
                 else:
                     try:
-                        prev = "AI: "+tr2en.translate(mail.parent().body)+"\n"
+                        prev = "Human 1: "+tr2en.translate(mail.parent().body)+"\n"
                     except:
                         prev = None
 
@@ -154,32 +159,31 @@ while True:
                         time.sleep(5)
                         continue
                     except deep_translator.exceptions.NotValidPayload:
-                        out = "..."
+                        out = ". . ."
                         item.reply(out)
                         item.mark_read()
                         continue
                     except deep_translator.exceptions.NotValidLength:
-                        out = "Bu ne amk çok uzun"
+                        out = "##bu ne amk çok uzun"
                         item.reply(out)
                         item.mark_read()
                         continue
+                    out = out.lower()
                     out = out.replace("numara", "hayır")
-                    out = out.replace("girişiniz için teşekkür ederiz","geri dönütünüz için teşekkür ederiz")
                     out = out.replace("ai ", "yapay zeka ")
-                    #time.sleep(15) #rate-limit?
+                    out = out.replace(" ai", " yapay zeka")
+                    out = out.replace("girişiniz için teşekkür ederiz","geri dönütünüz için teşekkür ederiz")
+                    time.sleep(15)
                     item.reply(slangify(out))
                 item.mark_read()
-    except praw.exceptions.RedditAPIException:
-        pass
+    except praw.exceptions.RedditAPIException as e:
+        print(e)
     except Exception as e:
         print(e)
 
     with open("posts_replied_to.txt", "w") as f:
         if len(posts_replied_to) > 0:
-            print("Saving Replied Posts...")
             to_remove = []
             for post_id in posts_replied_to:
                 f.write(post_id + "\n")
                 to_remove.append(post_id)
-            for post_id in to_remove:
-                posts_replied_to.remove(post_id)
